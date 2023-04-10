7e455af3e1f5e7c5e80ac68af906df1841033f0f
SoliDeoHoneretGloria <iphone_1st@naver.com>
2023-04-10 14:12:05 +0200

    Initial commit

---
+++ src/main/java/com/zerobase/gurumesi/GurumesiApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.gurumesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurumesiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GurumesiApplication.class, args);
+    }
+
+}
