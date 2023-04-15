9bdc6989c286ab514277a76476ca5c40bbb40c28
SoliDeoHoneretGloria <iphone_1st@naver.com>
2023-04-15 20:56:35 +0200

    Feat :Schedule function initial commit

---
+++ src/main/java/com/zerobase/gurumesi/book/scheduler/BookingCheckerScheduler.java
@@ -0,0 +1,9 @@
+package com.zerobase.gurumesi.book.scheduler;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class BookingCheckerScheduler {
+}
